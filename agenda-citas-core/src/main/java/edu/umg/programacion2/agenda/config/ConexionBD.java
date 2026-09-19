0222c6a6dd33f302361fce7e29fe188e35d7e31e
GISELLE ORTIZ CALVILLO <gortizc4@miumg.edu.gt>
2026-09-18 20:48:10 -0600

    Rama lista para el parcial 2

--- agenda-citas-core/src/main/java/edu/umg/programacion2/agenda/config/ConexionBD.java
+++ agenda-citas-core/src/main/java/edu/umg/programacion2/agenda/config/ConexionBD.java
@@ -9,7 +9,7 @@
 
 	    private static final String URL = "jdbc:mysql://localhost:3306/agenda_citas";
 	    private static final String USUARIO = "root";
-	    private static final String PASSWORD = "gigidb";
+	    private static final String PASSWORD = "Escriba su contraseña aqui";
 
 	    public static Connection obtenerConexion() throws SQLException {
 	        return DriverManager.getConnection(URL, USUARIO, PASSWORD);
